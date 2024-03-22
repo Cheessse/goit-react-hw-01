b7611cf2b42b6d2c979c6a4bf761113dd5d3a0aa
David <143638937+Cheessse@users.noreply.github.com>
2024-03-22 22:00:07 +0000

    update code

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,3 +1,22 @@
-const FriendsList = () => { }
+import FriendListItem from "../FriendListItem/FriendListItem"
+
+const FriendsList = ({ friends }) => { 
+    return (
+<ul>
+            {friends.map(friend => {
+                return (
+    <li key={friend.id}>
+        <FriendListItem
+         avatar={friend.avatar}
+         name={friend.name}
+         isOnline={friend.isOnline}
+        />
+	</li>
+        )
+    })}
+</ul>
+
+    )
+}
 
 export default FriendsList
